890a5fd60e778ba9206816cecab818af726783c2
moseskereya <lazaromoses77@gmail.com>
2023-07-16 13:46:39 +0300

    adding more hotels

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,10 +1,13 @@
 import React, { Component, createRef } from 'react';
 import { GoogleApiWrapper, Map, Marker, InfoWindow } from 'google-maps-react';
 import Hotel from "./Hotel"
+import {Autocomplete} from "@react-google-maps/api"
+
 class Maps extends Component {
   constructor(props) {
     super(props);
     this.mapRef = createRef();
+    this.searchInputRef = createRef();
   }
 
   state = {
@@ -13,7 +16,7 @@ class Maps extends Component {
     activeMarker: null,
     selectedPlace: null,
     zoom: 16,
-    autocomplete: ''
+    
   };
 
   componentDidMount() {
@@ -23,19 +26,21 @@ class Maps extends Component {
         (position) => {
           const { latitude, longitude } = position.coords;
           const currentLocation = { lat: latitude, lng: longitude };
-
+  
           const mapOptions = {
             center: currentLocation,
-            gestureHandling:"cooperative",
+            gestureHandling: "cooperative",
             zoom: 18,
           };
-
+  
           try {
             const map = new google.maps.Map(this.mapRef.current, mapOptions);
             const service = new google.maps.places.PlacesService(map);
+            const initialBounds = map.getBounds();
+  
             this.setState({ currentLocation, map, service });
-            this.fetchHotels(service, map.getBounds());
-
+            this.fetchHotels(service, initialBounds);
+  
             map.addListener('bounds_changed', () => {
               const newBounds = map.getBounds();
               if (newBounds) {
@@ -54,31 +59,81 @@ class Maps extends Component {
       console.error('Geolocation is not supported by this browser.');
     }
   }
-
   
 
-  fetchHotels(service, bounds) {
-    const request = {
-      bounds: bounds,
-      query: 'hotels',
-    };
 
-    service.textSearch(request, (results, status) => {
-      if (status === this.props.google.maps.places.PlacesServiceStatus.OK) {
-        const hotels = results.map((place) => ({
-          position: place.geometry.location,
-          name: place.name,
-          image: place.photos ? place.photos[0].getUrl() : null,
-          rating: place.rating ? place.rating : null,
-        }));
 
-        this.setState({ hotels });
-       console.log(this.state.hotels)
-      } else {
-        console.error('Places service request failed:', status);
+  handleSearch = () => {
+    const { google } = this.props;
+    const { map, service } = this.state;
+    const searchQuery = this.searchInputRef.current.value.trim();
+    if (map && service && searchQuery !== '') {
+      const request = {
+        query: searchQuery,
+      };
+  
+      service.textSearch(request, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          const hotels = results.map((place) => ({
+            position: place.geometry.location,
+            name: place.name,
+            image: place.photos ? place.photos[0].getUrl() : null,
+            rating: place.rating ? place.rating : null,
+          }));
+  
+          this.setState({ hotels });
+  
+          if (results.length > 0) {
+            const { geometry } = results[0];
+            const { location } = geometry;
+  
+            map.panTo(location);
+  
+            const { lat, lng } = location;
+            this.setState({ currentLocation: { lat: lat(), lng: lng() } });
+          } else {
+            console.warn('No results found for the search query:', searchQuery);
+          }
+        } else {
+          console.error('Places service request failed:', status);
+        }
+      });
+    }
+  };
+  
+  
+  
+
+fetchHotels(service, bounds, pagination) {
+  const request = {
+    bounds: bounds,
+    query: 'hotels',
+    ...pagination,
+  };
+
+  service.textSearch(request, (results, status, pagination) => {
+    if (status === this.props.google.maps.places.PlacesServiceStatus.OK) {
+      const hotels = results.map((place) => ({
+        position: place.geometry.location,
+        name: place.name,
+        image: place.photos ? place.photos[0].getUrl() : null,
+        rating: place.rating ? place.rating : null,
+      }));
+
+      this.setState((prevState) => ({
+        hotels: [...prevState.hotels, ...hotels],
+      }));
+
+      if (pagination.hasNextPage) {
+        pagination.nextPage();
       }
-    });
-  }
+    } else {
+      console.error('Places service request failed:', status);
+    }
+  });
+}
+
+  
 
   onMarkerClick = (props, marker) => {
     this.setState({
@@ -95,23 +150,29 @@ class Maps extends Component {
   };
 
   render() {
-    const { hotels, currentLocation, activeMarker, selectedPlace, zoom } = this.state
-      return (
-        <>
+    const { hotels, currentLocation, activeMarker, selectedPlace, zoom } = this.state;
+    const onLoad = (autoComplete) => this.handleSearch(autoComplete);
+    return (
+      <>
+        <div className='hotel_search'>
+        <Autocomplete  onLoad = {onLoad} onPlaceChanged={this.handleSearch}>
+          <input type='text' placeholder='Explore Hotels Now' ref={this.searchInputRef} />  
+       </Autocomplete>
+        </div>
         <div ref={this.mapRef} className="map_container">
           {currentLocation && (
-            <Map google={this.props.google} zoom={zoom} initialCenter={currentLocation}>
-             <Marker
-              position={currentLocation}
-              className="user-location-dot"
-              icon={{
-                url: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-                scaledSize: new this.props.google.maps.Size(40, 40),
-                anchor: new this.props.google.maps.Point(20, 40),
-                animation: this.props.google.maps.Animation.BOUNCE,
-              }}
-            />
-  
+            <Map google={this.props.google} zoom={zoom} center={currentLocation}>
+              <Marker
+                position={currentLocation}
+                className="user-location-dot"
+                icon={{
+                  url: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                  scaledSize: new this.props.google.maps.Size(40, 40),
+                  anchor: new this.props.google.maps.Point(20, 40),
+                  animation: this.props.google.maps.Animation.BOUNCE,
+                }}
+              />
+
               {hotels.map((marker, index) => (
                 <Marker
                   key={index}
@@ -122,7 +183,7 @@ class Maps extends Component {
                   rating={marker.rating}
                 />
               ))}
-  
+
               <InfoWindow marker={activeMarker} visible={Boolean(activeMarker)}>
                 <div>
                   {selectedPlace && (
@@ -139,15 +200,15 @@ class Maps extends Component {
           )}
         </div>
         <div className='hotels_list'>
-           {hotels?.map((hotel, index) => (
-             <Hotel key={index} hotel={hotel} />
-           ))}
-          </div>
-        </>
-      );
-    }
+          {hotels?.map((hotel, index) => (
+            <Hotel key={index} hotel={hotel} />
+          ))}
+        </div>
+      </>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAl5nMA3rjGiV58HYm1mSlqTmuBdkDmkMw', 
+  apiKey: 'AIzaSyAl5nMA3rjGiV58HYm1mSlqTmuBdkDmkMw',
 })(Maps);
